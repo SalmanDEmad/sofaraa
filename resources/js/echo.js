048e1a75d4993983bfb37749f43cb5dfbeab8028
Salman Emad <salmanemadulhoque2003@gmail.com>
2025-08-06 18:21:42 +0300

    fix echo

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,14 +1,15 @@
 import Echo from 'laravel-echo';
-
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
+
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    wsHost: import.meta.env.VITE_REVERB_HOST || (isLocalhost ? 'localhost' : window.location.hostname),
+    wsPort: isLocalhost ? 6001 : 80,
+    wssPort: isLocalhost ? 6001 : 443,
+    forceTLS: !isLocalhost,
     enabledTransports: ['ws', 'wss'],
 });
